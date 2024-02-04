29a00b3942bf1ec5ec2980e808d9ff55ff18c9de
Ankit Nonia <Ankit Nonia@LAPTOP-LDGBOJ3N>
2024-02-04 18:00:23 +0530

    initial commint

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ![simplinnovation](https://4.bp.blogspot.com/-f7YxPyqHAzY/WJ6VnkvE0SI/AAAAAAAADTQ/0tDQPTrVrtMAFT-q-1-3ktUQT5Il9FGdQCLcB/s350/simpLINnovation1a.png)
 
 # Basic CRUD: Flask & MySQL
+#sahi hai
 
 1. Activate __MongoDB__ server:
     
--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+
 from flask_cors import CORS
 import yaml
 
 app = Flask(__name__)
-config = yaml.load(open('database.yaml'))
-client = MongoClient(config['uri'])
-# db = client.lin_flask
-db = client['lin_flask']
+app.config["MONGO_URI"] = yaml.safe_load(open('database.yaml'))['uri']
+mongo = PyMongo(app)
 CORS(app)
 
 @app.route('/')
@@ -17,15 +15,13 @@ def index():
 
 @app.route('/data', methods=['POST', 'GET'])
 def data():
-    
     # POST a data to database
     if request.method == 'POST':
         body = request.json
         name = body['name']
         age = body['age']
 
-        # db.users.insert_one({
-        db['users'].insert_one({
+        mongo.db.users.insert_one({
             "name": name,
             "age": age
         })
@@ -34,65 +30,23 @@ def data():
             'name': name,
             'age': age
         })
-    
+
     # GET all data from database
     if request.method == 'GET':
-        allData = db['users'].find()
-        dataJson = []
-        for data in allData:
-            id = data['_id']
+        all_data = mongo.db.users.find()
+        data_json = []
+        for data in all_data:
+            user_id = str(data['_id'])
             name = data['name']
             age = data['age']
-            dataDict = {
-                'id': str(id),
+            data_dict = {
+                'id': user_id,
                 'name': name,
                 'age': age
             }
-            dataJson.append(dataDict)
-        print(dataJson)
-        return jsonify(dataJson)
+            data_json.append(data_dict)
 
-@app.route('/data/<string:id>', methods=['GET', 'DELETE', 'PUT'])
-def onedata(id):
-
-    # GET a specific data by id
-    if request.method == 'GET':
-        data = db['users'].find_one({'_id': ObjectId(id)})
-        id = data['_id']
-        name = data['name']
-        age = data['age']
-        dataDict = {
-            'id': str(id),
-            'name': name,
-            'age': age
-        }
-        print(dataDict)
-        return jsonify(dataDict)
-        
-    # DELETE a data
-    if request.method == 'DELETE':
-        db['users'].delete_many({'_id': ObjectId(id)})
-        print('\n # Deletion successful # \n')
-        return jsonify({'status': 'Data id: ' + id + ' is deleted!'})
-
-    # UPDATE a data by id
-    if request.method == 'PUT':
-        body = request.json
-        name = body['name']
-        age = body['age']
-
-        db['users'].update_one(
-            {'_id': ObjectId(id)},
-            {
-                "$set": {
-                    "name":name,
-                    "age":age
-                }
-            }
-        )
-
-        print('\n # Update successful # \n')
-        return jsonify({'status': 'Data id: ' + id + ' is updated!'})
+        return jsonify(data_json)
 
 if __name__ == '__main__':
     app.debug = True
