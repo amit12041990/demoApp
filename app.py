801db13005a6404379c689ebdcbb7732aff881df
Ankit Nonia <Ankit Nonia@LAPTOP-LDGBOJ3N>
2024-02-04 18:19:08 +0530

    creating demo app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
-
 from flask_cors import CORS
 import yaml
 
@@ -15,39 +14,25 @@ def index():
 
 @app.route('/data', methods=['POST', 'GET'])
 def data():
-    # POST a data to database
-    if request.method == 'POST':
-        body = request.json
-        name = body['name']
-        age = body['age']
+    try:
+        collection_name = 'users'
+        # Query the 'users' collection
+        data_from_collection = mongo.db[collection_name].find()
 
-        mongo.db.users.insert_one({
-            "name": name,
-            "age": age
-        })
+        # Convert MongoDB cursor to list of dictionaries
+        data_list = list(data_from_collection)
+
+        # If you want to return JSON response
+        return jsonify(data_list)
+        
+        # If you want to return HTML template with the data
+        # return render_template('your_template.html', data=data_list)
+    except Exception as e:
+        print("Error:", str(e))
         return jsonify({
-            'status': 'Data is posted to MongoDB!',
-            'name': name,
-            'age': age
+            'error': 'Failed to fetch data from MongoDB'
         })
-
-    # GET all data from database
-    if request.method == 'GET':
-        all_data = mongo.db.users.find()
-        data_json = []
-        for data in all_data:
-            user_id = str(data['_id'])
-            name = data['name']
-            age = data['age']
-            data_dict = {
-                'id': user_id,
-                'name': name,
-                'age': age
-            }
-            data_json.append(data_dict)
-
-        return jsonify(data_json)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(port=4000)
